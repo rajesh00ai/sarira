8451601f327c854c4bfe1577f563c8794e192290
rajesh00ai <rajesh007ai@gmail.com>
2025-08-03 12:10:57 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+import Story from './components/Story';
+import Recipes from './components/Recipes';
+import UsageIdeas from './components/UsageIdeas';
+import Blog from './components/Blog';
+import Gallery from './components/Gallery';
+import ShippingReturns from './components/ShippingReturns';
+import FAQ from './components/FAQ';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Story />
+      <Recipes />
+      <UsageIdeas />
+      <Blog />
+      <Gallery />
+      <ShippingReturns />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
